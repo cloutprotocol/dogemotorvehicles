814400f96be9606e200719450bb22c2e0c493505
cloutcoin <78846662+cloutprotocol@users.noreply.github.com>
2024-11-13 15:36:30 -0800

    vercel

--- api/chat.js
+++ api/chat.js
@@ -1,22 +1,48 @@
 import { Server } from "socket.io";
 
-export default function handler(req, res) {
+const users = new Set();
+const messages = [];
+
+export default function SocketHandler(req, res) {
   if (!res.socket.server.io) {
-    const io = new Server(res.socket.server);
-    res.socket.server.io = io;
+    const io = new Server(res.socket.server, {
+      path: '/socket.io/',
+      addTrailingSlash: false,
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+      }
+    });
 
-    io.on("connection", (socket) => {
-      console.log("New client connected");
+    io.on('connection', socket => {
+      console.log('Client connected');
 
-      socket.on("join line", (role) => {
-        console.log(`User joined as ${role}`);
-        // Handle join line logic
+      socket.on('join line', (role) => {
+        if (role !== 'viewer') {
+          users.add(socket.id);
+          socket.username = role;
+          socket.emit('chat history', messages);
+        }
+        io.emit('line update', users.size);
       });
 
-      socket.on("disconnect", () => {
-        console.log("Client disconnected");
+      socket.on('chat message', (msg) => {
+        if (socket.username && socket.username !== 'viewer') {
+          const message = { ...msg, id: socket.id };
+          messages.push(message);
+          io.emit('chat message', message);
+        }
+      });
+
+      socket.on('disconnect', () => {
+        if (socket.username && socket.username !== 'viewer') {
+          users.delete(socket.id);
+          io.emit('line update', users.size);
+        }
       });
     });
+
+    res.socket.server.io = io;
   }
   res.end();
 } 
